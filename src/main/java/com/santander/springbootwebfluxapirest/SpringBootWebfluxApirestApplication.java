37301b7361b80e88020a3d6de06a6fa9deea4dc6
Santander Ruiz <cesantander@indracompany.com>
2023-09-11 00:18:46 -0500

    Add Eureka Client

--- src/main/java/com/santander/springbootwebfluxapirest/SpringBootWebfluxApirestApplication.java
+++ src/main/java/com/santander/springbootwebfluxapirest/SpringBootWebfluxApirestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +17,7 @@ import com.santander.springbootwebfluxapirest.entity.Producto;
 import com.santander.springbootwebfluxapirest.service.ProductoService;
 
 @Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringBootWebfluxApirestApplication implements CommandLineRunner {
 
